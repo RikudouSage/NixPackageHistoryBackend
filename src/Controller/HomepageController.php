dce9f45829d56a2c4b824d1a1f15564a352174ed
Dominik Chrastecky <dominik@chrastecky.cz>
2023-11-13 00:03:17 +0100

    Add an endpoint with latest revision

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Package;
+use App\Repository\PackageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,6 +21,27 @@ final class HomepageController extends AbstractController
                 $urlGenerator->generate('app.packages.detail', ['package' => 'php'], referenceType: UrlGeneratorInterface::ABSOLUTE_URL),
                 $urlGenerator->generate('app.packages.version.detail', ['package' => 'php', 'version' => '8.2.11'], referenceType: UrlGeneratorInterface::ABSOLUTE_URL),
             ],
+        ]);
+    }
+
+    #[Route('/latest-revision', name: 'app.latest_revision', methods: [Request::METHOD_GET])]
+    public function lastRevision(PackageRepository $repository): JsonResponse
+    {
+        $latest = $repository->createQueryBuilder('p')
+            ->setMaxResults(1)
+            ->orderBy('p.datetime', 'DESC')
+            ->getQuery()
+            ->getSingleResult();
+        if (!$latest instanceof Package) {
+            return new JsonResponse([
+                'revision' => null,
+                'datetime' => null,
+            ]);
+        }
+
+        return new JsonResponse([
+            'revision' => $latest->getRevision(),
+            'datetime' => $latest->getDatetime()?->format('c'),
         ]);
     }
 }
