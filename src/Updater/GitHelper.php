3320927d0fbad127ce5e53d41a4cb356493ef98a
Dominik Chrastecky <dominik@chrastecky.cz>
2023-11-16 21:11:56 +0100

    Fetch all git revisions at once

--- src/Updater/GitHelper.php
+++ src/Updater/GitHelper.php
@@ -16,28 +16,25 @@ final readonly class GitHelper
     public function getRevisions(string $repositoryPath, string $startingCommit = 'HEAD'): iterable
     {
         $originalWorkingDirectory = getcwd() ?: throw new RuntimeException('Cannot get current working directory');
-        $commandTemplate = 'git rev-list --max-count=50 --format=format:"%%H %%cd" --date=iso-strict %s';
-        do {
-            $command = sprintf($commandTemplate, $startingCommit);
-            chdir($repositoryPath) || throw new InvalidArgumentException("The repository at '{$repositoryPath}' does not exist.");
-            $output = trim(shell_exec($command));
-            chdir($originalWorkingDirectory);
-            $lines = explode("\n", $output);
-            $i = 0;
-            foreach ($lines as $line) {
-                if ($i % 2 === 0) {
-                    ++$i;
-                    continue;
-                }
-                $parts = explode(" ", $line);
-                yield new GitRevision(
-                    revision: $parts[0],
-                    dateTime: (new DateTimeImmutable($parts[1]))->setTimezone(new DateTimeZone('UTC')),
-                );
-                $startingCommit = $parts[0];
+        $command = "git rev-list --format=format:\"%H %cd\" --date=iso-strict {$startingCommit}";
+        chdir($repositoryPath) || throw new InvalidArgumentException("The repository at '{$repositoryPath}' does not exist.");
+        $output = trim(shell_exec($command));
+        chdir($originalWorkingDirectory);
+        $lines = explode("\n", $output);
+
+        $i = 0;
+        foreach ($lines as $line) {
+            if ($i % 2 === 0) {
                 ++$i;
+                continue;
             }
-        } while (true);
+            $parts = explode(" ", $line);
+            yield new GitRevision(
+                revision: $parts[0],
+                dateTime: (new DateTimeImmutable($parts[1]))->setTimezone(new DateTimeZone('UTC')),
+            );
+            ++$i;
+        }
     }
 
     public function getRevision(string $repositoryPath, string $revision): GitRevision
