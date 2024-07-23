0d0ebbd82f279866c2bc80917573f85a0a06bdfe
Dominik Chrastecky <dominik@chrastecky.cz>
2024-07-23 13:48:46 +0200

    Use storage for stats

--- src/Controller/StatsController.php
+++ src/Controller/StatsController.php
@@ -2,19 +2,40 @@
 
 namespace App\Controller;
 
+use App\Enum\NamedSetting;
 use App\Repository\PackageRepository;
+use App\Service\Settings;
+use App\Service\Storage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class StatsController extends AbstractController
 {
     #[Route('/stats', name: 'app.stats')]
-    public function stats(PackageRepository $packageRepository): JsonResponse
-    {
-        return new JsonResponse([
+    public function stats(
+        PackageRepository $packageRepository,
+        #[Autowire('%app.storage.enable%')]
+        bool $useStorage,
+        Storage $storage,
+        Settings $settings,
+    ): Response {
+        $fresh = fn () => [
             'packages' => $packageRepository->getPackageCount(),
             'versions' => $packageRepository->getVersionCount(),
-        ]);
+        ];
+
+        if (!$useStorage) {
+            return new JsonResponse($fresh());
+        }
+
+        $key = "{$settings->getSetting(NamedSetting::LatestRevision)}/stats.json";
+        if (!$storage->exists($key)) {
+            $storage->createObject($key, json_encode($fresh()), 'application/json');
+        }
+
+        return $this->redirect($storage->getObjectLink($key));
     }
 }
